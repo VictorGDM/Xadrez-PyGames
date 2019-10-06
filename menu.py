787514c55644573666f59fc7ee9aaa41d019a5d3
Victor-01 <vgdiasmachado@gmail.com>
2019-10-06 04:02:16 -0300

    versao 1

--- menu.py
+++ menu.py
@@ -1,5 +1,5 @@
-import sysconfig, pygame
-import crgImg, idtBto, tcrTela, crgSons
+import sys, pygame
+import crgImg, idtBto, tcrTela
 
 ###-----------------------------------------------------------###
 """
@@ -8,54 +8,63 @@ idtBto  ===== Identifica as posições dos botões
 tcrTela ===== Troca as imagens da tela
 """
 ###-----------------------------------------------------------###
+def updateScreen(tela):
+    screen.blit(tela, (0, 0))
+    pygame.display.update()
 
 #--Variaveis de alteração--#
 pygame.init()
 size = width, height = 1000, 650
 screen = pygame.display.set_mode(size)
-# relogio = pygame.time.Clock()
 executando = True
-tSom = 0
-som = crgSons.mdrSom(tSom)
 botao = 0
 tema = 0
 
 #--Telas do jogo--#
 pygame.display.set_caption('Xadrez')
 camada = 0
-# crgImg.mdrTema(tema)
+crgImg.mdrTema(tema)
 tela = crgImg.menu
-# screen.blit(tela, (0,0))
+updateScreen(tela)
 
 #--Jogo em execução--#
 while executando == True:
     x, y = pygame.mouse.get_pos()
-    crgSons.mdrSom(tSom)
-    tela, botao, tSom = idtBto.btMouse(x, y, camada, tela)
 
     #--Controlar os eventos--#
     for event in pygame.event.get():
-        
+
         if event.type == pygame.QUIT:
             executando = False
-        
+
         if event.type == pygame.MOUSEBUTTONUP:
-            if camada == 4:
-                tela, camada, tema = tcrTela.prxTela(camada, botao, tema, tSom)
+            if camada == 4:                
+                if botao == 0 or botao == 5:
+                    mudou, tela, camada = tcrTela.prxTela(camada, botao, tema)
+                
+                else:
+                    mudou, tela, camada, tema = tcrTela.prxTela(camada, botao, tema)
+                    crgImg.mdrTema(tema)
+                    updateScreen(tela)
 
             elif camada == 0 and botao == 5:
-                executando = tcrTela.prxTela(camada, botao, tema, tSom)
+                executando = tcrTela.prxTela(camada, botao, tema)
 
             else:
-                tela, camada = tcrTela.prxTela(camada, botao, tema, tSom)                
-                
-    # pygame.time.delay(1000)
-    # relogio.tick(10)
-    print(f'x:{x} y:{y} b:{botao} t:{tema} c:{camada} tS:{tSom}')
+                mudou, tela_r, camada_r = tcrTela.prxTela(camada, botao, tema)    
+                if(mudou):
+                    tela = tela_r
+                    camada = camada_r
+                    updateScreen(tela)
 
-    # redim = pygame.transform.smoothscale(tela, size)
-    screen.blit(tela, (0,0))
-    pygame.display.update()
+    print(f'x:{x} y:{y} b:{botao} t:{tema} c:{camada}')
+
+    mudou, tela_r, botao_r = idtBto.btMouse(x, y, camada, tela)
+    
+    if(mudou):
+        tela = tela_r
+        botao = botao_r
+        updateScreen(tela)
 
 pygame.quit()
-sysconfig.exit()
+sys.exit()
